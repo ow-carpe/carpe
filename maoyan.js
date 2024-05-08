b8fe4d37ecd59547637487aaa5aba6d613211f15
ow-carpe <62401218+ow-carpe@users.noreply.github.com>
2024-05-08 11:49:11 +0800

    Update maoyan.js

--- maoyan.js
+++ maoyan.js
@@ -3,14 +3,14 @@
 项目名称：彩云天气-净化/解锁SVIP
 下载地址：https://t.cn/A66d95hV
 更新日期：2024-04-12
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
+脚本作者：carpebody
+电报频道：https://t.me/carpebody
 使用声明：⚠️仅供参考，🈲转载与售卖！
 
 **************************************
 
 [rewrite_local]
-^https:\/\/yanchu\.maoyan\.com\/myshow\/ajax\/v2\/performance url script-response-body https://raw.githubusercontent.com/ow-carpe/carpe/master/maoyan.js
+^https:\/\/yanchu\.maoyan\.com\/myshow\/ajax\/v2 url script-response-body https://raw.githubusercontent.com/ow-carpe/carpe/master/maoyan.js
 
 [mitm]
 hostname = yanchu.maoyan.com
@@ -19,12 +19,12 @@ hostname = yanchu.maoyan.com
 
 
 const chxm1024 = {};
-const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const carpebody = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const url = $request.url;
 const adUrl = /(activity\?app_name|operation\/banners)/;
 const tcUrl = /conditions/;
 const vipUrl = /https:\/\/yanchu\.maoyan\.com\/myshow\/ajax\/v2\/performance/;
-const userUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/v\d\/user\?app_name/;
+const userUrl = /https:\/\/yanchu\.maoyan\.com\/myshow\/ajax\/v2\/show/;
 const syUrl = /trial_card\/info/;
 const qyUrl = /entries/;
 const peUrl = /privileges/;
@@ -36,40 +36,44 @@ if (typeof $response == "undefined") {
 } else {
   switch (true) {
     case adUrl.test(url):
-      chxm1023.status = "ok";
-      chxm1023.activities = [{"items":[{}]}];
-      chxm1023.data = [];
+      carpebody.status = "ok";
+      carpebody.activities = [{"items":[{}]}];
+      carpebody.data = [];
       break;
     case tcUrl.test(url):
-      chxm1023.actions = [];
-      chxm1023.popups = [];
+      carpebody.actions = [];
+      carpebody.popups = [];
       break;
     case vipUrl.test(url):
   
-      chxm1023.data.saleStatus = 3;
+      carpebody.data.saleStatus = 3;
       break;
     case userUrl.test(url):
-      chxm1023.result = { ...chxm1023.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 1,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...chxm1023.result.wt,  vip: {  ...chxm1023.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 1,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  };
+      carpebody.data.hasInventory = true;
+       carpebody.data.data.forEach(item => {
+            item.salesPlanVO.hasInventory = true;
+            item.salesPlanVO.sellStatus = 3;
+        });
       break;
     case syUrl.test(url):
-      chxm1023.receive_status = 0;
-      chxm1023.vip_type = "svip";
-      chxm1023.activated_at = 1712600671;
-      chxm1023.vip_duration = "999";
-      chxm1023.expired_at = 4092599349;
-      chxm1023.has_valid_card = 0;
+      carpebody.receive_status = 0;
+      carpebody.vip_type = "svip";
+      carpebody.activated_at = 1712600671;
+      carpebody.vip_duration = "999";
+      carpebody.expired_at = 4092599349;
+      carpebody.has_valid_card = 0;
       break;
     case qyUrl.test(url):
-      chxm1023["entries"] = [{  "url" : "https://t.me/chxm1023",  "id" : 1,  "name" : "叮当猫",  "type" : 1,  "pos" : 2  }];
+      carpebody["entries"] = [{  "url" : "https://t.me/carpebody",  "id" : 1,  "name" : "叮当猫",  "type" : 1,  "pos" : 2  }];
       break;
     case peUrl.test(url):
-      chxm1023["privileges"] = [{  "vip_type" : "svip",  "subscription_chat_quota" : 999  }];
+      carpebody["privileges"] = [{  "vip_type" : "svip",  "subscription_chat_quota" : 999  }];
       break;
     case topUrl.test(url):
-      chxm1023["data"] = [{  "badge_type" : "",  "title" : "叮当猫",  "url" : "https://t.me/chxm1023",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm2.png"  },...chxm1023.data];
+      carpebody["data"] = [{  "badge_type" : "",  "title" : "叮当猫",  "url" : "https://t.me/carpebody",  "feature_type" : "",  "avatar" : "https://raw.githubusercontent.com/carpebody/Script_X/main/icon/ddm2.png"  },...carpebody.data];
       break;
     }
-  chxm1024.body = JSON.stringify(chxm1023);
+  chxm1024.body = JSON.stringify(carpebody);
 }
 
 $done(chxm1024);
