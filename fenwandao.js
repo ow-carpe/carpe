7b5158a187b82da74c294abcb18c1e02a979a8fa
ow-carpe <62401218+ow-carpe@users.noreply.github.com>
2024-05-08 23:12:18 +0800

    Update fenwandao.js

--- fenwandao.js
+++ fenwandao.js
@@ -2,9 +2,9 @@
 项目名称：
 **************************************
 [rewrite_local]
-^https:\/\/yanchu\.maoyan\.com\/myshow\/ajax url script-response-body https://raw.githubusercontent.com/ow-carpe/carpe/master/maoyan.js
+^https:\/\/api\.livelab\.com\.cn\/performance\/app url script-response-body https://raw.githubusercontent.com/ow-carpe/carpe/master/fenwandao.js
 [mitm]
-hostname = yanchu.maoyan.com
+hostname = api.livelab.com.cn
 *************************************/
 
 
@@ -13,10 +13,10 @@ const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body ||
 var body = $response.body;
 var url = $request.url;
 
-if (url.includes("yanchu.maoyan.com") && body) {
+if (url.includes("api.livelab.com.cn") && body) {
   var obj = JSON.parse($response.body);
-  if (url.includes("/myshow/ajax/v2/performance") && obj.data) {
-    obj.data.saleStatus = 3;
+  if (url.includes("app/project/get_project_info") && obj.data) {
+    obj.data.buttonStatus = 1;
   }
   if (url.includes("/myshow/ajax/v2/show") && obj.data) {
     obj.data.forEach(item => {
