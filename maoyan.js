761dff293b20183150d33692275f7f8364bdc2d1
ow-carpe <62401218+ow-carpe@users.noreply.github.com>
2024-05-08 10:40:08 +0800

    Update maoyan.js

--- maoyan.js
+++ maoyan.js
@@ -1,20 +1,2 @@
-const url = $request.url;
-const body = $response.body;
-
-if (url.includes("https://yanchu.maoyan.com/myshow/ajax/v2/performance")) {
-    try {
-        const data = JSON.parse(body);
-        if (data && Array.isArray(data.data)) {
-            data.data.forEach(item => {
-                if (item.saleStatus === 5) {
-                    item.saleStatus = 3;
-                }
-            });
-            $done({ body: JSON.stringify(data) });
-        }
-    } catch (error) {
-        console.log("Error:", error.message);
-    }
-} else {
-    $done({});
-}
+[rewrite_local]
+^https://yanchu.maoyan.com/myshow/ajax/v2/performance url-response-body "saleStatus\": 5" "saleStatus\": 3"
